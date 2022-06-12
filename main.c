35a144691905269688fb98b986a0d8fdc4788958
longwatermelon <longwatermelon1@gmail.com>
2022-06-12 15:51:03 -0700

    init and quit sdl in example

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 int main(int argc, char **argv)
 {
+    SDL_Init(SDL_INIT_VIDEO);
+
     SDL_Window *w = SDL_CreateWindow("Libras demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
     SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
@@ -66,6 +68,8 @@ int main(int argc, char **argv)
 
     SDL_DestroyRenderer(r);
     SDL_DestroyWindow(w);
+
+    SDL_Quit();
 
     return 0;
 }
